2513aedb959069b6872f74ba88a3f6ebf88d8efa
omkar <ombulbule.ob@gmailcom>
2023-03-12 19:46:53 +0530

    new file

---
+++ Inventory_Final_Project/Inventory_Final_Project/inventory-management-spring-boot-master/Inventory_Management/src/test/java/com/inventoryManagement/inventoryManagement.java
@@ -0,0 +1,13 @@
+package com.inventoryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class inventoryManagement {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
